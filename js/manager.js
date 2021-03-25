d09e7edccf339810e7014c2682fb91bc08f8e673
nadine-shi <nadinshi@gmail.com>
2021-03-25 10:46:41 +0300

    Implement Document editing (frontend)

--- js/manager.js
+++ js/manager.js
@@ -4,6 +4,8 @@ module.exports = function (oAppData) {
 	var
 		_ = require('underscore'),
 
+		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+
 		App = require('%PathToCoreWebclientModule%/js/App.js'),
 
 		CAbstractFileModel = require('%PathToCoreWebclientModule%/js/models/CAbstractFileModel.js')
@@ -21,6 +23,27 @@ module.exports = function (oAppData) {
 						if (oView && _.isFunction(oView.registerCreateButtonsController))
 						{
 							oView.registerCreateButtonsController(require('modules/%ModuleName%/js/views/AddFileButtonView.js'));
+						}
+					}
+				});
+				App.subscribeEvent('FilesWebclient::ParseFile::after', function (aParams) {
+					var
+						oFile = aParams[0],
+						oData = aParams[1]
+					;
+					if (oFile.hasAction('edit'))
+					{
+						oFile.removeAction('edit');
+						if (oFile.oActionsData['edit'])
+						{
+							oFile.actions.unshift('edit');
+							oFile.oActionsData['edit'].Text = TextUtils.i18n('%MODULENAME%/ACTION_EDIT_FILE');
+							oFile.oActionsData['edit'].HandlerName = 'viewFile';
+						}
+						if (oFile.hasAction('view'))
+						{
+							oFile.removeAction('view');
+							oFile.actions.push('view');
 						}
 					}
 				});
